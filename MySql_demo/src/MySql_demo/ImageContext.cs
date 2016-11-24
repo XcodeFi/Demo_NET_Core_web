9065ff085e0667a9f3488a9e147e545e1967609e
Administrator <Administrator@DESKTOP-34MP92L>
2016-11-24 21:01:05 +0700

    mysql

---
+++ MySql_demo/src/MySql_demo/ImageContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MySql_demo
+{
+    public class ImageContext:DbContext
+    {
+        public ImageContext(DbContextOptions<ImageContext> option):base(option)
+        {
+
+        }
+    }
+
+    public static class ImageContextFactory
+    {
+        public static ImageContext Create(string connectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ImageContext>();
+            optionsBuilder.UseMySql(connectionString);
+
+            //Ensure database creation
+            var context = new ImageContext(optionsBuilder.Options);
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+    }
+
+    public class ImageTb
+    {
+        public ImageTb()
+        {
+        }
+
+        public int Id { get; set; }
+
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [MaxLength(50)]
+        public string Url { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# Demo_NET_Core_web
-web_demo
